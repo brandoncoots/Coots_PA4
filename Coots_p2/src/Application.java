e427c360b4ffbe56752f330c2e0aecb88fcfe34b
brandoncoots <55032041+brandoncoots@users.noreply.github.com>
2019-11-03 22:43:09 -0500

    Add files via upload

---
+++ Coots_p2/src/Application.java
@@ -0,0 +1,11 @@
+public class Application
+{
+	public static void main(String[] args)
+	{
+	  String input_file = "problem2.txt";
+	  String output_file = "unique_word_counts.txt";
+	  DuplicateCounter DupCouObj = new DuplicateCounter();
+	  DupCouObj.count(input_file);
+	  DupCouObj.write(output_file);
+	}
+}
